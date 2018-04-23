5068b0caebb310122911021f504de19b1bc0f699
chensong zhu <mzhu@deputy.com>
2018-04-23 22:47:14 +1000

    deck Entry

--- components/DeckEntry.js
+++ components/DeckEntry.js
@@ -7,23 +7,30 @@ import * as flashCardApi  from '../utils/api'
 
 class NewDeck extends Component{
     state ={
-        deckName:''
+        name:'asdf',
+        cardsNumbers: 123
     }
-    submit =() =>{
+    addCard =() =>{
         (this.state.deckName) && flashCardApi.submitDeck(this.state.deckName)
     }
+    startQuiz =() =>{
+
+    }
     render(){
+        const {name, cardsNumbers} = this.state
         return(
             <View>
-                <Text style={styles.title}>What is the title of your new deck?</Text>
-                <TextInput
-                    style={styles.content}
-                    onChangeText={(deckName) => this.setState({deckName})}
-                    value={this.state.deckName}
-                    placeholder='Deck title'
-                />
-                <TextButton style={{margin: 20}} onPress={this.submit}>
-                    Submit
+                <Text style={styles.deckName}>
+                    {name}
+                </Text>
+                <Text style={styles.deckCardNumber}>
+                    cards number: {cardsNumbers}
+                </Text>
+                <TextButton style={{margin: 20}} onPress={this.addCard}>
+                    Add Card
+                </TextButton>
+                <TextButton style={{margin: 20}} onPress={this.startQuiz}>
+                    Start Quiz
                 </TextButton>
             </View>
         )
@@ -31,22 +38,25 @@ class NewDeck extends Component{
 }
 
 const styles = StyleSheet.create({
-    title: {
+    deckName: {
         fontSize: 50,
         paddingTop: 50,
-        paddingBottom: 50,
         justifyContent: 'center',
         alignItems: 'center',
         marginLeft: 10,
         marginRight: 10,
         textAlign: 'center'
     },
-    content:{
-        height: 30,
-        borderColor: 'gray',
-        borderWidth: 1,
+    deckCardNumber:{
+        fontSize: 20,
+        paddingTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
         marginLeft: 10,
         marginRight: 10,
+        textAlign: 'center'
     }
 })
+
+
 export default NewDeck
